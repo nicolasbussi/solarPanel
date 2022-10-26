ef710dc8897313a27fc7ea6a375db4236cae34d0
Nicolas Bussi <nicolas.bussi@outlook.com>
2022-10-26 16:34:34 +0200

    new template

--- src/App.js
+++ src/App.js
@@ -1,118 +1,146 @@
-import React, {Component} from 'react';
-// import NavBar from './components/navbar';
+import React from "react";
+import GlobalStyles from 'styles/GlobalStyles';
+import { css } from "styled-components/macro"; //eslint-disable-line
 
-// import PAGES from './constant/pages';
-// import HomePage from './pages/home_page';
-// import EnergySavingPage from './pages/energy_saving_page';
-// import MaintainancePage from './pages/maintainance_page';
-// import AirConditioningPage from './pages/air_conditioning_page';
-// import AirFiltrationPage from './pages/air_filtration_page';
-// import WaterFiltrationPage from './pages/water_filtration_page';
-// import WaterHeatersPage from './pages/water_heaters_page';
-// import InsulationAndDuctworkPage from './pages/insulation_and_ductwork_page';
-// import ReferralProgramPage from './pages/referral_program_page';
-// import CareersPage from './pages/careers_page';
-// import BookOnlinePage from './pages/book_online_page';
-// import BookServicePage from './pages/book_service_page';
-// import FinancingPage from './pages/financing_page';
+/*
+ * This is the entry point component of this project. You can change the below exported default App component to any of
+ * the prebuilt landing page components by uncommenting their import and export lines respectively.
+ * See one of the landing page components to better understand how to import and render different components (Always
+ * make sure if you are building your own page, the root component should be the AnimationRevealPage component. You can
+ * disable the animation by using the disabled prop.
+ *
+ * The App component below is using React router to render the landing page that you see on the live demo website
+ * and the component previews.
+ *
+ */
 
-import Navbar from './components/navbar';
-import Home from "./pages/home_page"
-import AboutUsPage from "./pages/about_us_page"
-import ServicesPage from "./pages/services_page"
-import ContactPage from "./pages/contact_page"
-import { Route, Routes } from "react-router-dom"
-import FinancingPage from './pages/financing_page';
+/* Use AnimationRevealPage as a wrapper component for your pages if you are building a custom one yourself */
+// import AnimationRevealPage from "helpers/AnimationRevealPage.js";
+
+/*
+ * Hero section is the top most section on the page. It contains the header as well.
+ * So you dont need to import headers
+ * separately
+ */
+
+// import Hero from "components/hero/TwoColumnWithVideo.js";
+// import Hero from "components/hero/TwoColumnWithInput.js";
+// import Hero from "components/hero/TwoColumnWithFeaturesAndTestimonial.js";
+// import Hero from "components/hero/TwoColumnWithPrimaryBackground.js";
+// import Hero from "components/hero/FullWidthWithImage.js";
+// import Hero from "components/hero/BackgroundAsImage.js";
+// import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
+
+// import Features from "components/features/ThreeColSimple.js";
+// import Features from "components/features/ThreeColWithSideImage.js";
+// import Features from "components/features/ThreeColWithSideImageWithPrimaryBackground.js";
+// import Features from "components/features/VerticalWithAlternateImageAndText.js";
+// import Features from "components/features/DashedBorderSixFeatures";
+// import MainFeature from "components/features/TwoColWithButton.js";
+// import MainFeature from "components/features/TwoColSingleFeatureWithStats.js";
+// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats2.js";
+// import MainFeature from "components/features/TwoColWithTwoHorizontalFeaturesAndButton.js";
+// import FeatureWithSteps from "components/features/TwoColWithSteps.js";
+// import FeatureStats from "components/features/ThreeColCenteredStatsPrimaryBackground.js";
+
+// import Pricing from "components/pricing/ThreePlans.js";
+// import Pricing from "components/pricing/ThreePlansWithHalfPrimaryBackground.js";
+// import Pricing from "components/pricing/TwoPlansWithDurationSwitcher.js";
+
+// import SliderCard from "components/cards/ThreeColSlider.js";
+// import TrendingCard from "components/cards/TwoTrendingPreviewCardsWithImage.js";
+// import Portfolio from "components/cards/PortfolioTwoCardsWithImage.js";
+// import TabGrid from "components/cards/TabCardGrid.js";
+
+// import Blog from "components/blogs/ThreeColSimpleWithImage.js";
+// import Blog from "components/blogs/ThreeColSimpleWithImageAndDashedBorder.js";
+// import Blog from "components/blogs/PopularAndRecentBlogPosts.js";
+// import Blog from "components/blogs/GridWithFeaturedPost.js";
+
+// import Testimonial from "components/testimonials/TwoColumnWithImage.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndProfilePictureReview.js";
+// import Testimonial from "components/testimonials/TwoColumnWithImageAndRating.js";
+// import Testimonial from "components/testimonials/ThreeColumnWithProfileImage.js";
+// import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
+
+// import FAQ from "components/faqs/SimpleWithSideImage.js";
+// import FAQ from "components/faqs/SingleCol.js";
+// import FAQ from "components/faqs/TwoColumnPrimaryBackground.js";
+
+// import ContactUsForm from "components/forms/SimpleContactUs.js";
+// import ContactUsForm from "components/forms/TwoColContactUsWithIllustration.js";
+// import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.js";
+//
+// import GetStarted from "components/cta/GetStarted.js";
+// import GetStarted from "components/cta/GetStartedLight.js";
+// import DownloadApp from "components/cta/DownloadApp.js";
+
+// import Footer from "components/footers/SimpleFiveColumn.js";
+// import Footer from "components/footers/FiveColumnWithInputForm.js";
+// import Footer from "components/footers/FiveColumnWithBackground.js";
+// import Footer from "components/footers/FiveColumnDark.js";
+// import Footer from "components/footers/MiniCenteredFooter.js";
+
+/* Ready Made Pages (from demos folder) */
+// import EventLandingPage from "demos/EventLandingPage.js";
+// import HotelTravelLandingPage from "demos/HotelTravelLandingPage.js";
+// import AgencyLandingPage from "demos/AgencyLandingPage.js";
+// import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+// import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
+// import ServiceLandingPage from "demos/ServiceLandingPage.js";
+// import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
+
+/* Inner Pages */
+// import LoginPage from "pages/Login.js";
+// import SignupPage from "pages/Signup.js";
+// import PricingPage from "pages/Pricing.js";
+// import AboutUsPage from "pages/AboutUs.js";
+// import ContactUsPage from "pages/ContactUs.js";
+// import BlogIndexPage from "pages/BlogIndex.js";
+// import TermsOfServicePage from "pages/TermsOfService.js";
+// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+
+import ComponentRenderer from "ComponentRenderer.js";
+import MainLandingPage from "MainLandingPage.js";
+import ThankYouPage from "ThankYouPage.js";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  // If you want to disable the animation just use the disabled `prop` like below on your page's component
+  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
 
 
-class App extends Component {
-  
-//   state   = {
-//     page: PAGES.HOME
-// }
-
-// handleSelectPage = (selectedPage) => { 
-//   const newPage = selectedPage
-//   this.setState({page: newPage})
-// };
-
-
-  render() {
-
-    // let selectedPage = this.state.page;
-
-    // get the correct content page to display
-    // let content = getContentToShow(selectedPage);
-    
-    
-    return (
-      <>
-        {/* <NavBar onSelectPage= {this.handleSelectPage} selectedPage={this.state.page}/>
-        <div className="container">
-        <h1 style={{paddingLeft: '185px', color: '#BADA55', textAalign:'center', fontSize:'18px'}}>1-888-788-4822</h1>
-        </div>
-        {content} */}
-        <Navbar />
-      <div className="container">
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/financing" element={<FinancingPage />} />
+          <Route path="/components/:type/:subtype/:name" element={<ComponentRenderer />} />
+          <Route path="/components/:type/:name" element={<ComponentRenderer />} />
+          <Route path="/thank-you" element={<ThankYouPage />} />
+          <Route path="/" element={<MainLandingPage />} />
         </Routes>
-      </div>
-      </>
-    )
-  }
+      </Router>
+    </>
+  );
 }
 
-export default App;
+// export default EventLandingPage;
+// export default HotelTravelLandingPage;
+// export default AgencyLandingPage;
+// export default SaaSProductLandingPage;
+// export default RestaurantLandingPage;
+// export default ServiceLandingPage;
+// export default HostingCloudLandingPage;
 
+// export default LoginPage;
+// export default SignupPage;
+// export default PricingPage;
+// export default AboutUsPage;
+// export default ContactUsPage;
+// export default BlogIndexPage;
+// export default TermsOfServicePage;
+// export default PrivacyPolicyPage;
 
-//  function getContentToShow(selectedPage){
-//   let content;
-//   if(selectedPage === PAGES.HOME){
-//     content =  <HomePage/>
-
-//   } else if(selectedPage === PAGES.ENERGY_SAVING){
-//     content =  <EnergySavingPage/>
-
-//   } else if(selectedPage === PAGES.MAINTAINANCE){
-//     content =  <MaintainancePage/>
-
-//   } else if(selectedPage === PAGES.AIR_CONDITIONING){
-//     content =  <AirConditioningPage/>
-
-//   } else if(selectedPage === PAGES.AIR_FILTRATION){
-//     content =  <AirFiltrationPage/>
-
-//   } else if(selectedPage === PAGES.WATER_FILTRATION){
-//     content =  <WaterFiltrationPage/>
-
-//   } else if(selectedPage === PAGES.WATER_HEATERS){
-//     content =  <WaterHeatersPage/>
-
-//   } else if(selectedPage === PAGES.INSULATION_AND_DUCTWORK){
-//     content =  <InsulationAndDuctworkPage/>
-
-//   } else if(selectedPage === PAGES.REFERRAL_PROGRAM){
-//     content =  <ReferralProgramPage/>
-
-//   } else if(selectedPage === PAGES.CAREERS){
-//     content =  <CareersPage/>
-
-//   } else if(selectedPage === PAGES.BOOK_ONLINE){
-//     content =  <BookOnlinePage/>
-
-//   } else if(selectedPage === PAGES.BOOK_SERVICE){
-//     content =  <BookServicePage/>
-
-//   } else if(selectedPage === PAGES.FINANCING){
-//     content =  <FinancingPage/>
-
-//   }
-
-//   return content
-
-//  }
+// export default MainLandingPage;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./styles.css"
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import Modal from "react-modal";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+Modal.setAppElement("#root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
